126ad37168ebfe6af17b5ddea4ad81b4e18d156e
Martin Orrego <martinorregosilva@uc.cl>
2021-07-06 19:17:24 -0400

    productos

---
+++ Sites/Productos/ver_productos.php
@@ -0,0 +1,49 @@
+<?php
+require('../config/conexion.php');
+include('../templates/header.html');
+include('../templates/navbar.php');
+
+$query = "SELECT nombre, id FROM productos;";
+$resultado = $db2 -> prepare($query);
+$resultado -> execute();
+$data = $resultado -> fetchAll();
+?>
+
+<div align='center'>
+    <h1> Ver tod0s los productos </h1>
+</div>
+
+<div>
+    <?php
+        echo "<br><br>
+        <table align='center' cellspacing='5em'>
+            <thead>
+                <tr>
+                    <th><h2> Producto </h2></th>";
+        if (True) {
+            echo "<th><h2> Detalles </h2></th>";
+        }
+        echo "</tr>";
+        foreach ($data as $d) {
+            echo "
+                <tr>
+                    <td> $d[0] $d[1]</td>
+            ";
+            if (true) {
+                echo "
+                <td>
+                <form action='visitar_tienda.php' method='post' align='center'>
+                    <input type='hidden' value=$d[1] class='boton' name='tienda_elegida'>
+                    <input type='submit' value='Visitar' class='boton'>
+                </form>
+                </td>
+                ";
+            }
+            echo "</tr>";
+        }
+        echo "
+            </thead>
+            <tbody>
+            ";
+    ?>
+</div>

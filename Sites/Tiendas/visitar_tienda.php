fc9acb2db7a93f41b5ee6a01d64d61291259ede8
Martin Orrego <martinorregosilva@uc.cl>
2021-07-07 19:47:26 -0400

    Rutas

--- Sites/Tiendas/visitar_tienda.php
+++ Sites/Tiendas/visitar_tienda.php
@@ -260,7 +260,7 @@ $d = $data[0];
                     echo "</select>";
                     echo "<br>";
                     echo "<br>Cantidad <br>";
-                    echo "<input type='number' class='boton' name='cantidad' min='1' max='50'>";
+                    echo "<input type='number' class='boton' name='cantidad' min='1' max='50' value='1'>";
                     echo "<input type='hidden' value=$tienda_id class='boton' name='tienda_elegida'>";
                     echo "<br>";
                     echo "<br>";

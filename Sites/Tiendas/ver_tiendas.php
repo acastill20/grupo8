3ab603b0af33f6fc825132a092cb76b428ecf383
Bea2000 <beatriz.errazuriz@uc.cl>
2021-07-07 20:22:45 -0400

    diseñooooooo

--- Sites/Tiendas/ver_tiendas.php
+++ Sites/Tiendas/ver_tiendas.php
@@ -13,16 +13,18 @@ $data = $resultado -> fetchAll();
 <html style="background-color: #6495ed">
 <br>
 <br>
+<div style="width: 70%; padding-top: 2%; padding-bottom: 2%; background-color: white; margin-left: 15%; margin-right: 15%">
 <div align='center'>
-    <h1 class='is-size-1'> Ver todas las tiendas </h1>
+    <h1 class='is-size-1'> Nuestras tiendas ! </h1>
 </div>
+</div
 
 <div>
     <?php
         echo "<br><br>
         <table class='table' align='center' cellspacing='5em'>
             <thead>
-                <tr>
+                <tr class='is-selected'>
                     <th><h2><strong> ID </strong></h2></th>
                     <th><h2><strong> Tienda </strong></h2></th>";
         if(!isset($_SESSION['rut'])) {
@@ -40,14 +42,14 @@ $data = $resultado -> fetchAll();
             echo "
                 <tr>
                     <td> $d[1]</td>
-                    <td> $d[0]</td>
-            ";
+                    <td>" ?> <?php echo ucwords($d[0]) ?> </td>
+            <?php ;
             if (isset($_SESSION['rut'])) {
                 echo "
                 <td>
                 <form action='visitar_tienda.php' method='post' align='center'>
-                    <input type='hidden' value=$d[1] class='boton' name='tienda_elegida'>
-                    <input type='submit' value='Visitar' class='boton'>
+                    <input type='hidden' value=$d[1] class='button is-info' name='tienda_elegida'>
+                    <input type='submit' value='Visitar' class='button is-info is-small is-rounded'>
                 </form>
                 </td>
                 ";
